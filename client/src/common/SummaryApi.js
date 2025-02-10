de59e00090ea6ad7657eebf91bec32e147c56727
Abenewolde <abnetwoldedev@gmail.com>
2025-02-10 10:04:04 +0300

    chnage to render

--- client/src/common/SummaryApi.js
+++ client/src/common/SummaryApi.js
@@ -1,5 +1,5 @@
 // export const baseURL = "http://localhost:8080"
-export const baseURL = "https://backend-api-ecommerce-tg-mini-app-bot.vercel.app"
+export const baseURL = "https://api-tgweb.onrender.com"
 
 const SummaryApi = {
     register : {
